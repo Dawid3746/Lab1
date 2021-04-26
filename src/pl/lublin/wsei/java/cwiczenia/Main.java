c7f6f4598d75841a6227b4bc00dd6662e2f3a254
Dawid Sałek <dawidsalekst@gmail.com>
2021-04-26 16:18:05 +0200

    Punkt 12 Dodanie własnej klasy Account

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args) {
 
         Account ac = new Account();
-        ac.setName("Dawid Sałek");
+        ac.setName("dawid Sałek");
         System.out.println(ac.getName());
 
 
--- src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
+++ src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
@@ -1,18 +1,20 @@
 package pl.lublin.wsei.java.cwiczenia.mylib;
 
-public class Account {
+import org.apache.commons.lang3.StringUtils;
 
+public class Account {
     private String name;
 
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String s){
+        String[] tokens = StringUtils.split(s);
+
+        for (int i = 0; i < tokens.length; i++){
+            tokens[i] = StringUtils.capitalize(tokens[i]);
+        }
+
+        name = StringUtils.join(tokens, ' ');
     }
-
-
-    public String getName() {
+    public String getName(){
         return name;
     }
-
-
-
 }
