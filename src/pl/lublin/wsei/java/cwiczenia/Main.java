a26ceca180150593de3c0c7650023038e3cb734b
Dawid Sałek <dawidsalekst@gmail.com>
2021-04-26 14:35:44 +0200

    7. System.out.printf

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -10,5 +10,11 @@ public class Main {
         System.out.print("Kot\n");
         System.out.print("ma\n");
         System.out.print("Ale\n");
+
+        int a = 2;
+        double b = 3.14;
+        String s = " przykładowy tekst";
+
+        System.out.printf("a = %d, b = %.2f, s=%20s %n",a,b,s);
     }
 }
