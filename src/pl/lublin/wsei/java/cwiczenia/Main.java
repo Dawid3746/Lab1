2ddf2785d40f39d39977c8700a9aebad5abd7095
Dawid Sałek <dawidsalekst@gmail.com>
2021-04-26 14:29:07 +0200

    7. System.out.println() z wykorzystaniem znacznika /n

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -6,12 +6,7 @@ public class Main {
      * @param args argumenty funkcji main
      */
     public static void main(String[] args){
-        System.out.print("Kot");
-        System.out.print("ma");
-        System.out.print("Ale");
-        System.out.println("Kot");
-        System.out.println("ma");
-        System.out.println("Ale");
+
         System.out.print("Kot\n");
         System.out.print("ma\n");
         System.out.print("Ale\n");
