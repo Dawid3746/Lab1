44ee1bcdd169b2435bb855660cebb71851cf80e9
Dawid Sałek <dawidsalekst@gmail.com>
2021-04-26 15:30:56 +0200

    Punkt 10

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -10,37 +10,30 @@ public class Main {
      */
     public static void main(String[] args) {
 
-        int a = 2;
-        double b = 3.14;
-        String s = " przykładowy tekst";
-
-        System.out.printf("a = %d, b = %.2f, s=%20s %n", a, b, s);
-
-        System.out.println("Nazywaliśmy to \"Witamina B3\"");
-
-        System.out.println("\nalfa\tsin(alfa)");
-        for (int i = 0; i < 370; i += 10) {
-            System.out.printf("%d\t%f\n", i, Math.sin(i / 360.0 * 2 * Math.PI));
-        }
-
-        int sum = 0;
-        System.out.println("\narg\tlog2(arg)\tsum(arg)");
-        for (int i = 1; i < 32; i *= 2) {
-            sum += i;
-            System.out.printf("%d\t%.2f\t\t%d\n", i, (Math.log(i) / Math.log(2)), sum);
-        }
 
         Scanner inp = new Scanner(System.in);
         int x, y;
         do {
-            System.out.print("Podaj pierwsza liczbe: ");
+            System.out.print("Podaj liczbe: ");
             x = inp.nextInt();
             if (x == 0) break;
-            System.out.print("Podaj druga liczbe: ");
-            y = inp.nextInt();
-            if (y == 0) break;
-            System.out.printf("Wynik dodawania %d + %d = %d%n", x, y, x + y);
+
+            System.out.printf("dec=%s\nhex=%s\nbin=%s \n".toUpperCase(),
+                    x,
+                    leftPad(Integer.toHexString(x), '0', 4),
+                    leftPad(Integer.toBinaryString(x), '0', 4));
+
         } while (true);
 
+
+    }
+
+    public static String leftPad(String aText, char aChar, int aWidth) {
+        String res = aText;
+        for (int i = 0; i < aWidth; i += 1) {
+            res = aChar + res;
+        }
+
+        return res;
     }
 }
