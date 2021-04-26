7768887837e0130220daf6b203fb2f1ed9241a60
Dawid Sałek <dawidsalekst@gmail.com>
2021-04-26 17:29:55 +0200

    Punkt 12 Podłączenie biblioteki i dodanie funkcji translit

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -13,9 +13,8 @@ public class Main {
      */
     public static void main(String[] args) {
 
-        Account ac = new Account();
-        ac.setName("dawid Sałek");
-        System.out.println(ac.getName());
+        String str = Account.translit("привітання");
+        System.out.println(str);
 
 
     }
--- src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
+++ src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
@@ -5,16 +5,42 @@ import org.apache.commons.lang3.StringUtils;
 public class Account {
     private String name;
 
-    public void setName(String s){
+    private static String ukrAlphabet = "абвгдеєжзиіїйклмнопрстуфхцчшщьюя";
+    private static String translitRepl[] = {"a", "b", "v", "g", "d", "e", "je", "zh", "z", "y", "i", "ji", "j",
+            "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "h", "c",
+            "ch", "sh", "shh", "'", "ju", "ja"};
+
+
+    public void setName(String s) {
+        name = capitalize(s);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static String capitalize(String s) {
         String[] tokens = StringUtils.split(s);
 
-        for (int i = 0; i < tokens.length; i++){
+        for (int i = 0; i < tokens.length; i++) {
             tokens[i] = StringUtils.capitalize(tokens[i]);
         }
-
-        name = StringUtils.join(tokens, ' ');
+        return StringUtils.join(tokens, ' ');
     }
-    public String getName(){
-        return name;
+
+    public static String translit(String arg) {
+        arg = arg.toLowerCase();
+
+        String translit = "";
+        for (int i = 0; i < arg.length(); i++) {
+            int idx = StringUtils.indexOf(ukrAlphabet, arg.charAt(i));
+            if (idx == -1) {
+                translit += arg.charAt(i);
+            } else {
+                translit += translitRepl[idx];
+            }
+        }
+        return translit;
+
     }
 }
