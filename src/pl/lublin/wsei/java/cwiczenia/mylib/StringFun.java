861792a866129c43741835f5829cd611b8e7ef93
Dawid Sałek <dawidsalekst@gmail.com>
2021-05-06 02:37:00 +0200

    Utworzenie klasy StringFun, metody Anarchize i AnarchizeTest

---
+++ src/pl/lublin/wsei/java/cwiczenia/mylib/StringFun.java
@@ -0,0 +1,23 @@
+package pl.lublin.wsei.java.cwiczenia.mylib;
+
+import org.apache.commons.lang3.StringUtils;
+
+
+
+public class StringFun {
+
+    public static String anarchize(String str){
+
+        String res = "";
+
+        for (int i = 0; i < str.length(); i++){
+
+            if (Character.isUpperCase(str.charAt(i))){
+                res += Character.toLowerCase(str.charAt(i));
+            } else {
+                res += Character.toUpperCase(str.charAt(i));
+            }
+        }
+        return res;
+    }
+}
