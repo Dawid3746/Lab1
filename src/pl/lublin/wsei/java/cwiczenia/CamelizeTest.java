8712b8d8cecd6acbed94c22a8bed67781cf8f56f
Dawid Sałek <dawidsalekst@gmail.com>
2021-05-06 02:48:34 +0200

    Metoda Camelize i jej test

---
+++ src/pl/lublin/wsei/java/cwiczenia/CamelizeTest.java
@@ -0,0 +1,14 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+import pl.lublin.wsei.java.cwiczenia.mylib.StringFun;
+
+
+
+public class CamelizeTest {
+
+    public static void main(String[] args){
+
+        System.out.println(StringFun.camelize("KRÓTKA zabawna Funkcja"));
+
+    }
+}
